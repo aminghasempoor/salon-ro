3327d12d78ec6dd1fd034bd79e47603f05ba560b
AminGhasempoor <aminghasempoor79@gmail.com>
2024-09-09 08:46:47 +0330

    install shadcn and zod

---
+++ src/app/[locale]/page.js
@@ -0,0 +1,13 @@
+import {useTranslations} from "next-intl";
+import {Link} from "@/i18n/routing";
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+    const t = useTranslations();
+  return (
+      <div>
+          <h1>{t('HomePage.title')}</h1>
+          <Button><Link href="/about">{t('HomePage.about')}</Link></Button>
+      </div>
+  );
+}
---
+++ src/i18n/routing.js
@@ -0,0 +1,12 @@
+import {defineRouting} from 'next-intl/routing';
+import {createSharedPathnamesNavigation} from 'next-intl/navigation';
+
+export const routing = defineRouting({
+    // A list of all locales that are supported
+    locales: ['en', 'fa'],
+
+    // Used when no locale matches
+    defaultLocale: 'fa'
+});
+
+export const {Link, redirect, usePathname, useRouter} = createSharedPathnamesNavigation(routing);
