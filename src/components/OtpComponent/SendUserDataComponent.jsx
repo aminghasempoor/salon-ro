cf2f2a52915bebfa49bc8b44e86bccbcfdec54fa
dopehr <sepehrnotron@gmail.com>
2024-11-25 15:25:37 +0330

    minor edit

--- src/components/OtpComponent/SendUserDataComponent.jsx
+++ src/components/OtpComponent/SendUserDataComponent.jsx
@@ -78,7 +78,7 @@ export default function SendUserDataComponent({
                 <Form {...form}>
                     <form
                         onSubmit={form.handleSubmit(onSubmit)}
-                        className="mt-16 gap-4 flex flex-col h-full w-full lg:max-w-[400px]   items-center  pb-8 justify-between lg:justify-center lg:gap-16"
+                        className="mt-16  flex flex-col h-full w-full lg:max-w-[400px]   items-center  pb-8 justify-center gap-16"
                     >
                         <div className="w-full flex flex-col gap-16">
                             <div className="w-full flex flex-col place-self-start gap-4 items-center">
