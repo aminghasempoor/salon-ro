0ee9f964328b83ee0966289325549ba4e069f6fe
dopehr <sepehrnotron@gmail.com>
2024-11-25 16:24:35 +0330

    landing initial

---
+++ src/components/FirstPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstPageComponent = () => {
+  return (
+    <div className='h-full w-full p-8'>
+      hi
+    </div>
+  )
+}
+
+export default FirstPageComponent
