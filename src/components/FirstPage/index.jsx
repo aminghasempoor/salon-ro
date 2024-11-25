fa741c6ac273990b51bd1b6a1c64c4e5e40e78a4
dopehr <sepehrnotron@gmail.com>
2024-11-25 20:11:21 +0330

    FirstPageComponents

--- src/components/FirstPage/index.jsx
+++ src/components/FirstPage/index.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import Header from "./Header";
+import FirstScene from "./FirstScene";
+import Recommends from "./Recommends";
 
 const FirstPageComponent = () => {
-  return (
-    <div className='h-full w-full p-8'>
-      hi
-    </div>
-  )
-}
+    return (
+        <div className="relative w-full h-full flex flex-col bg-Light-BackgroundColor dark:bg-Dark-BackgroundColor px-4">
+            <Header />
 
-export default FirstPageComponent
+            <FirstScene />
+            <Recommends />
+            <div className=" w-full h-[300svh] bg-black/50">Download App </div>
+            <div>Reviews </div>
+            <div>Browse By City </div>
+            <div>Footer </div>
+        </div>
+    );
+};
+
+export default FirstPageComponent;
