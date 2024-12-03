224c3353c85a851df559972442b6056c5a7eef2e
dopehr <sepehrnotron@gmail.com>
2024-12-03 10:45:40 +0330

    text changes

--- src/components/FirstPage/FirstScene.jsx
+++ src/components/FirstPage/FirstScene.jsx
@@ -7,18 +7,18 @@ const FirstScene = () => {
     const t = useTranslations();
 
     return (
-        <div className="flex flex-col w-full lg:h-[calc(100svh-72px)] h-svh justify-center items-center text-Light-TextColor dark:text-Dark-TextColor  ">
+        <div className="flex flex-col w-full lg:h-[calc(100svh-72px)] h-svh justify-center items-center text-Light-TextColor dark:text-Dark-TextColor font-Vazirmatn ">
          
-                <h1 className="font-black text-[1.75rem] text-center">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                <h1 className="font-bold text-[2rem] text-center">
+                راه آراستگی بهت نزدیکتر شده. نوبتت رو بگیر.
                 </h1>
                 <div className="flex flex-col w-full h-fit justify-center items-center gap-4">
                     <div className="w-full h-[280px] mt-6 p-6 rounded-lg shadow-[inset_0px_0px_0px_6px_rgba(246,_246,_246,_1)]  ">
                         form
                     </div>
-                    <div className="text-[.75rem] font-bold">
-                        {" "}
-                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                    <div className="text-[1.5rem] font-extrabold">
+            
+                    وقتت مهم ترین داراییه!
                     </div>
                 </div>
                 <Button
