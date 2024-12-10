2c1f438d85117df7d1c290ddebf353699db26a47
dopehr <sepehrnotron@gmail.com>
2024-12-10 12:56:54 +0330

    more animation

---
+++ src/components/FirstPage/Services.jsx
@@ -0,0 +1,166 @@
+import ServiceCarousel from "@/core/components/carousel/ServiceCarousel";
+import React from "react";
+import { useSwiper } from "swiper/react";
+import { Button } from "../ui/button";
+
+const Services = () => {
+    return (
+        <div className="flex flex-col w-full h-fit items-center justify-center mt-4 gap-4"
+        // flex flex-col w-full h-fit items-center justify-center mt-4 gap-4
+        // flex flex-col w-full  px-4 py-4  z-0 justify-start items-start text-Light-TextColor dark:text-Dark-TextColor rounded-[16px] gap-4 shadow-[0px_0px_8px_0px_rgba(0,_0,_0,_0.2)] shadow-Light-Sec100/30 my-4
+        >
+            <h3 className="text-[1.5rem] font-bold">بر اساس خدمات</h3>
+            <ServiceCarousel slides={slides1} />
+        </div>
+    );
+};
+
+export default Services;
+
+// Dummy slides
+
+const slides1 = [
+    {
+        image: "https://img.freepik.com/premium-vector/woman-wearing-yellow-jacket-straw-hat-with-sunglasses-her-face_905719-6855.jpg?w=649",
+        title: "زنانه",
+    },
+    {
+        image: "https://img.freepik.com/free-vector/brunette-man-with-sunglasses_24908-81405.jpg?t=st=1733814692~exp=1733818292~hmac=410e2e9d7a2d93bc793fca7b319110e8e4c0eca980202e3987f3734c5d728391&w=900",
+        title: "مردانه",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/soothing-body-massage-logo_63220-158.jpg?w=900",
+        title: "ماساژ",
+    },
+    {
+        image: "https://img.freepik.com/premium-photo/hands-young-woman-with-dark-red-manicure-nails_87742-25317.jpg?w=649",
+        title: "ناخن",
+    },
+    {
+        image: "https://img.freepik.com/free-vector/realistic-jade-roller-gua-sha-set_23-2148850721.jpg?t=st=1733815981~exp=1733819581~hmac=496be426e06ba675194218809ec2b77487b6bedc8b45367b2ea7d5eba4e5079c&w=900",
+        title: "Skin Care",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/syringe-pharmaceutical-medical-minimal-vector-logos_186868-277.jpg?w=649",
+        title: "کلینیک زیبایی",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/handdrawn-womans-luxurious-eye-with-perfectly-shaped-eyebrows-full-lashes_491281-119.jpg?w=649",
+        title: "چشم و ابرو",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/composition-accessories-aroma-massage-spa-treatments_69317-535.jpg?w=649",
+        title: "SPA",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/young-woman-having-anti-aging-massage-beauty-facial-procedure-spa-therapy-vector-illustration_178650-14225.jpg?w=649",
+        title: "Facial",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/cream-photo-epilator-icon-cartoon-vector-cosmetic-body-care_98402-128771.jpg?w=649",
+        title: "لیزر موهای زائد",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/watercolor-abstract-feather-isolated-white-vivid-colours-gradient-technique-intricate-details-vector-graphic_236756-509.jpg?w=649",
+        title: "خدمات غیر تهاجمی",
+    },
+    {
+        image: "https://img.freepik.com/free-vector/pinup-style-lip-print_53876-8546.jpg?t=st=1733816969~exp=1733820569~hmac=2f464bebc8bab0049b9b05ccd7c795999a8305e6083a36fe03f0c98423f079cd&w=900",
+        title: "میکاپ",
+    },
+    {
+        image: "https://img.freepik.com/free-vector/hand-drawn-tattoo-gun-drawing-illustration_23-2150909290.jpg?t=st=1733817285~exp=1733820885~hmac=ddbe736dc76dfdf5412146576654d77c76e49e928f14ed98a9a3258b3949a438&w=649",
+        title: "تتو و پیرسینگ",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/bride-white-wedding-dress-standing-as-newlywed-just-married-female-vector-illustration_178650-56158.jpg?w=649",
+        title: "عروس",
+    },
+    {
+        image: "https://img.freepik.com/premium-vector/elegant-man-groom-married-image-vector-illustration-isolated_1322206-68001.jpg?w=649",
+        title: "داماد",
+    },
+    {
+        image: "https://img.freepik.com/free-vector/boy-happy-childhood-icon-isolated_24640-134167.jpg?t=st=1733817717~exp=1733821317~hmac=06ac5aedd014a21159521f25f9e78e49fae7db64810d086d9dbfeeb4a665ddcf&w=740",
+        title: "کودک",
+    },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+    // {
+    //     image: "https://picsum.photos/200/200",
+    //     title: " لورم ایپسوم",
+    // },
+   
+    // ... more slides
+];
+
+/* 
+ <div className="flex flex-wrap items-center justify-center gap-2  w-fit h-fit    my-8">
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+                <div className="flex flex-col  h-40 w-36 items-center gap-2 border ">
+                    <div className="w-[100px] aspect-square rounded-full bg-gray-500"></div>
+                    <div className="text-center font-normal text-[0.75rem]"> شینیون</div>
+                </div>
+            </div>
+*/
--- src/core/components/carousel/BasicCarousel.jsx
+++ src/core/components/carousel/BasicCarousel.jsx
@@ -15,8 +15,9 @@ import "./basicStyles.css";
 import useWindowSize from "../windowSize/WindowSize";
 import { Button } from "@/components/ui/button";
 
-// Disables carousel navigation on smaller Displays
 const BasicCarousel = ({ slides }) => {
+    const [swiperRef, setSwiperRef] = useState(null);
+    // Disables carousel navigation on smaller Displays
     const { width } = useWindowSize();
     const [navigationEnable, setNavigationEnable] = useState(true);
     useEffect(() => {
@@ -26,48 +27,63 @@ const BasicCarousel = ({ slides }) => {
             setNavigationEnable(false);
         }
     }, [width]);
+
     return (
-        <Swiper
-            speed={800}
-            className=" "
-            dir="ltr"
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={8}
-            slidesPerView={"auto"}
-            navigation={navigationEnable}
-            pagination={{ clickable: true }}
-            a11y={true}
+        <>
+            <Swiper
+                onSwiper={setSwiperRef}
+                style={{ paddingBottom: "40px" }}
+                className="select-none"
+                speed={800}
+                dir="ltr"
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={8}
+                slidesPerView={"auto"}
+                navigation={navigationEnable}
+                pagination={{ clickable: true }}
+                a11y={true}
 
-            // scrollbar={{ draggable: true }}
-            // onSwiper={(swiper) => console.log(swiper)}
-            // onSlideChange={() => console.log("slide change")}
-        >
-            {slides?.map((slide, index) => (
-                <SwiperSlide key={index} className="absolute items-center  ">
-                    <div className="relative  flex flex-col border rounded-2xl  overflow-hidden h-full ">
-                        <img src={slide.image} alt={`Slide ${index + 1}`} className=" " />
-                        <div className="flex flex-col p-4 w-full gap-2 text-[.75rem]   " dir="rtl">
-                            <h2 className="text-2xl font-bold ">{slide.title}</h2>
-                            <p className="w-full h-12  text-gray-700  overflow-hidden text-ellipsis ">
-                                {slide?.description}
-                            </p>
-                            <p className="w-full h-12  text-gray-700 line-clamp-1 overflow-hidden text-ellipsis  ">
-                                {slide?.address}
-                            </p>
+                // scrollbar={{ draggable: true }}
+                // onSwiper={(swiper) => console.log(swiper)}
+                // onSlideChange={() => console.log("slide change")}
+            >
+                {slides?.map((slide, index) => (
+                    <SwiperSlide
+                        key={index}
+                        className="absolute items-center  "
+                        style={{ height: "400px", width: "80%", maxWidth: "320px", padding: "0px 4px 0px 4px" }}
+                    >
+                        <div className="relative  flex flex-col border rounded-[16px]  overflow-hidden h-full ">
+                            <div className="overflow-hidden">
+                                <img src={slide.image} alt={`Slide ${index + 1}`} className="basic" loading="lazy"/>
+                            </div>
+                            <div className="flex flex-col p-4 w-full gap-2 text-[.75rem]   " dir="rtl">
+                                <h6 className="textClamp2 text-xl font-bold ">{slide.title}</h6>
+                                <p className="textClamp2 w-full h-12  text-gray-700  overflow-hidden text-ellipsis ">
+                                    {slide?.description}
+                                </p>
+                                <p className="textClamp2 w-full h-12  text-gray-700 line-clamp-1 overflow-hidden text-ellipsis  ">
+                                    {slide?.address}
+                                </p>
 
-                            <div className="flex w-full h-fit   text-gray-700   ">
-                                {/* خدماتی که ارائه میدهند */}
-                                {slide?.services?.map((service, i) => {
-                                    <p className="text-black text-[1rem] " key={i}>
-                                        {service}
-                                    </p>;
-                                })}
+                                <div className="flex w-full h-fit   text-gray-700   ">
+                                    {/* خدماتی که ارائه میدهند */}
+                                    {slide?.services?.map((service, i) => {
+                                        <p className="textClamp2 text-black text-[1rem] " key={i}>
+                                            {service}
+                                        </p>;
+                                    })}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+                    </SwiperSlide>
+                ))}
+                {/* <div className="absolute flex w-full justify-between ">
+                    <Button onClick={() => swiperRef.slidePrev()}>{"<"}</Button>
+                    <Button onClick={() => swiperRef.slideNext()}>{">"}</Button>
+                </div> */}
+            </Swiper>
+        </>
     );
 };
 
