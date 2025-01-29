dff1563140792421076413dec2dffc824501c689
AminGhasempoor <aminghasempoor79@gmail.com>
2025-01-29 11:41:29 +0330

    debug and build command

--- src/app/[locale]/register/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import OtpComponent from "@/components/OtpComponent";
-
-const page = () => {
-    return <OtpComponent />;
-};
-
-export default page;
--- src/core/components/carousel/BasicCarousel.jsx
+++ src/core/components/carousel/BasicCarousel.jsx
@@ -1,23 +1,17 @@
 "use client";
 import React, { useEffect, useState } from "react";
-// import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-
 import "./basicStyles.css";
 import useWindowSize from "../windowSize/WindowSize";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const BasicCarousel = ({ slides }) => {
     const [swiperRef, setSwiperRef] = useState(null);
-    // Disables carousel navigation on smaller Displays
     const { width } = useWindowSize();
     const [navigationEnable, setNavigationEnable] = useState(true);
     useEffect(() => {
@@ -42,10 +36,6 @@ const BasicCarousel = ({ slides }) => {
                 navigation={navigationEnable}
                 pagination={{ clickable: true }}
                 a11y={true}
-
-                // scrollbar={{ draggable: true }}
-                // onSwiper={(swiper) => console.log(swiper)}
-                // onSlideChange={() => console.log("slide change")}
             >
                 {slides?.map((slide, index) => (
                     <SwiperSlide
@@ -55,7 +45,7 @@ const BasicCarousel = ({ slides }) => {
                     >
                         <div className="relative  flex flex-col border rounded-[16px]  overflow-hidden h-full ">
                             <div className="overflow-hidden">
-                                <img src={slide.image} alt={`Slide ${index + 1}`} className="basic" loading="lazy" />
+                                <Image src={slide.image} alt={`Slide ${index + 1}`} width={200} height={200} className="basic" />
                             </div>
                             <div className="flex flex-col p-4 w-full gap-2 text-[.75rem]   " dir="rtl">
                                 <h6 className="textClamp2 text-xl font-bold ">{slide.title}</h6>
@@ -67,7 +57,6 @@ const BasicCarousel = ({ slides }) => {
                                 </p>
 
                                 <div className="flex w-full h-fit   text-gray-700   ">
-                                    {/* خدماتی که ارائه میدهند */}
                                     {slide?.services?.map((service, i) => {
                                         <p className="textClamp2 text-black text-[1rem] " key={i}>
                                             {service}
@@ -78,13 +67,8 @@ const BasicCarousel = ({ slides }) => {
                         </div>
                     </SwiperSlide>
                 ))}
-                {/* <div className="absolute flex w-full justify-between ">
-                    <Button onClick={() => swiperRef.slidePrev()}>{"<"}</Button>
-                    <Button onClick={() => swiperRef.slideNext()}>{">"}</Button>
-                </div> */}
             </Swiper>
         </>
     );
 };
-
 export default BasicCarousel;
